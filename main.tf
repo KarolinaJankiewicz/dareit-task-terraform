5beef77482f53024f3577cbf495d76e2ab0aef98
KarolinaJankiewicz <125274679+KarolinaJankiewicz@users.noreply.github.com>
2023-04-03 15:34:04 +0200

    add bucket

--- main.tf
+++ main.tf
@@ -23,3 +23,9 @@ resource "google_compute_instance" "dareit-vm-ci" {
   }
 }
 
+resource "google_storage_bucket" "kosiaszka-dareit" {
+  name          = "kosiaszka-dareit"
+  location      = "us-central1"
+  storage_class = '"STANDARD"
+}
+
